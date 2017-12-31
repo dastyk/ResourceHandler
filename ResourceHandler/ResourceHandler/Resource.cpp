1f3f2718d86c41e206babc3eafa31a5c765c840f
dastyk <dastykill@gmail.com>
2017-12-31 13:09:27 +0100

    Added some asserts and removed old commented code

--- ResourceHandler/ResourceHandler/Resource.cpp
+++ ResourceHandler/ResourceHandler/Resource.cpp
@@ -4,17 +4,12 @@
 
 namespace ResourceHandler
 {
-	//Resource::Resource(Utilz::GUID guid, ResourceHandler_Interface* resourceHandler)
-	//	: resourceHandler(resourceHandler), myGUID(guid), checkInCount(0)
-	//{
-	//}
 
 	Resource::~Resource()
 	{
-		_ASSERT_EXPR(resourceHandler, "A resource never got coupled with a resource handler");
+		_ASSERT_EXPR(resourceHandler, "A handler has not been created.");
 		if (checkInCount)
 		{
-			checkInCount = 1;
 			resourceHandler->CheckOut(*this);
 		}
 			
@@ -22,85 +17,40 @@ namespace ResourceHandler
 
 	DECLDIR_RH void Resource::Load()
 	{
+		_ASSERT_EXPR(resourceHandler, "A handler has not been created.");
 		resourceHandler->LoadResource(*this);
 	}
 
 	DECLDIR_RH void Resource::CheckIn()
 	{
+		_ASSERT_EXPR(resourceHandler, "A handler has not been created.");
 		if (++checkInCount == 1)
 			resourceHandler->CheckIn(*this);
 	}
 
 	DECLDIR_RH void Resource::CheckOut()
 	{
+		_ASSERT_EXPR(resourceHandler, "A handler has not been created.");
 		if (--checkInCount == 0)
 			resourceHandler->CheckOut(*this);
 	}
 
 	DECLDIR_RH uint32_t Resource::GetReferenceCount() const
 	{
+		_ASSERT_EXPR(resourceHandler, "A handler has not been created.");
 		return resourceHandler->GetReferenceCount(*this);
 	}
 
 	DECLDIR_RH LoadStatus Resource::GetData(ResourceDataVoid & data)
 	{
+		_ASSERT_EXPR(resourceHandler, "A handler has not been created.");
 		return resourceHandler->GetData(*this, data);
 	}
 
 	DECLDIR_RH LoadStatus Resource::PeekStatus() const
 	{
+		_ASSERT_EXPR(resourceHandler, "A handler has not been created.");
 		return resourceHandler->PeekStatus(*this);
 	}
 
-
-
-/*
-	LoadStatus Resource::PeekStatus()
-	{
-		return LoadStatus();
-	}
-
-	LoadStatus Resource::GetStatus()
-	{
-		if(checkInCount)
-			return resourceHandler->GetStatus(myGUID);
-		return LoadStatus::NOT_CHECKED_IN;
-	}
-
-	LoadStatus Resource::GetData(ResourceData& data)
-	{
-		if (checkInCount)
-			return resourceHandler->GetData(myGUID,data);
-		return LoadStatus::NOT_CHECKED_IN;
-	}
-
-	void Resource::CheckIn()
-	{
-		if(!checkInCount)
-			resourceHandler->CheckIn(myGUID);
-		checkInCount++;
-		return void();
-	}
-
-	void Resource::CheckOut()
-	{
-		if (!checkInCount)
-			resourceHandler->CheckOut(myGUID);
-		checkInCount--;
-		return void();
-	}
-
-	size_t Resource::GetReferenceCount() const
-	{
-		return resourceHandler->GetReferenceCount(myGUID);
-	}
-
-	Resource::Resource(Utilz::GUID guid, Utilz::GUID type)
-	{
-	}
-
-	Utilz::GUID Resource::Type()const
-	{
-		return Utilz::GUID();
-	}*/
 }
